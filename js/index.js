af777779a4e434f6113276f2356d0f1b75ad5de3
yeonding <yeonding2@naver.com>
2023-07-21 19:00:57 +0900

    [0721] tippy 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,13 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    document.querySelector(".morning").onclick = () => {
-        const body = document.body
-        if (body.classList.contains("morning-style")) {
-            body.classList.remove("morning-style");
-            document.querySelector(".morning").innerHTML=`morning🌞`
-        } else {
-            body.classList.add("morning-style");
-            document.querySelector(".morning").innerHTML=`night🌚`
-        }
+    const moons = document.querySelectorAll('.moon')
+    const mnBtn = document.querySelector('.morning')
+    let isMorning = false
+    const parts = document.querySelectorAll('part')
+    
+    const tippyOptions = {
+        placement: 'bottom'
+        ,theme: 'grey'
+        ,allowHTML: true
+        ,animation: 'shift-away'
     }
+
+
+    function showTippy(event, text) {
+        tippy(event, {
+            content: `<p class="tp ft1">${text}</p>`
+            , ...tippyOptions
+        })
+    }
+
+    mnBtn.onclick = () => {
+        const body = document.body
+        isMorning = !isMorning
+        body.classList.toggle('morning-style', isMorning)
+        mnBtn.innerHTML = isMorning ? 'night 🌚' : 'morning 🌞'
+    }
+
+    const titles = ['about me', 'skills', 'projects', 'archive', 'contact'];
+
+    moons.forEach((moon, index) => {
+        moon.addEventListener('mouseenter', () => {
+            if (index <= titles.length) {
+                showTippy(moon, titles[index]);
+            } 
+        }) // mouseenter end
+
+        moon.onclick = () => {
+            console.log ('clicked')
+        }
+    })
+
+
 })
+
+
